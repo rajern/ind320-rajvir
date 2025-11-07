d57efcf8b15ceb592363239733754eccfadaeaa0
Rajvir <rajvir.singh.aujla@nmbu.no>
2025-11-07 08:27:00 +0100

    Updated code to use plotly instead of matplotlib

--- pages/3_Plot explorer.py
+++ pages/3_Plot explorer.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
 from data_loader import load_open_meteo
 
 st.title("Plot explorer")
@@ -27,17 +27,31 @@ end_p   = pd.Period(end_label,   freq="M")
 mask = (df.index.to_period("M") >= start_p) & (df.index.to_period("M") <= end_p)
 d = df.loc[mask]
 
-# ---- Plot ----
-fig, ax = plt.subplots(figsize=(10, 4))
+# ---- Plot using plotly ----
 if choice == "All columns":
-    d.plot(ax=ax)
-    ax.set_title("All columns")
-    ax.set_ylabel("value")
+    fig = px.line(
+        d,
+        x=d.index,
+        y=d.columns,
+        title="All columns",
+        labels={"value": "Value", "variable": "Variable", "x": "Time"},
+    )
 else:
-    d[choice].plot(ax=ax)
-    ax.set_title(choice)
-    ax.set_ylabel(choice)
+    fig = px.line(
+        d,
+        x=d.index,
+        y=choice,
+        title=choice,
+        labels={"value": choice, "x": "Time"},
+    )
 
-ax.set_xlabel("time")
-ax.grid(True)
-st.pyplot(fig)
+fig.update_layout(
+    template="plotly_white",
+    xaxis_title="Time",
+    yaxis_title="Value",
+    legend_title_text="",
+    margin=dict(l=20, r=20, t=40, b=20),
+    height=450,
+)
+
+st.plotly_chart(fig, use_container_width=True)
